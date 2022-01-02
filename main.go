38f24b476172bc6bd911d3c22334a85a4b5fae18
christophberger <christophberger@users.noreply.github.com>
2022-01-02 22:44:03 +0100

    fix: change default pin to ofiicial one change url to CHANGE_ME (no default host name or IP address availabvle)

--- main.go
+++ main.go
@@ -117,8 +117,8 @@ func eventLoop(a *app, fs *fsapi.Fsapi) error {
 }
 
 func main() {
-	url := flag.String("url", "http://k--che.fritz.box/fsapi", "API URL to 3sixty")
-	pin := flag.String("pin", "0000", "PIN of 3sixty")
+	url := flag.String("url", "http://CHANFGE_ME/fsapi", "API URL to 3sixty")
+	pin := flag.String("pin", "1234", "PIN of 3sixty")
 	flag.Parse()
 	fs := fsapi.New(*url, *pin)
 	err := fs.CreateSession()
